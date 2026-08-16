a0c4b5f05ccc9e69967967afe3caa4a4adff2756
Yashavantha shetty <shettyyashavanth40@gmail.com>
2026-08-16 22:38:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+ const wrapper = document.querySelector(".sliderwrapper");
+ const menuItems = document.querySelectorAll(".menuItems");
+ 
+
+ menuItems.forEach((item,index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+    });
+ });
